da6c3450281695c52efa60d8e1b59b6ccbb1f92d
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-11 21:09:08 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,87 @@
 import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import { router as registration } from './registration.js';
 
 dotenv.config();
 
-const {
-  PORT: port = 3000,
-} = process.env;
-
 const app = express();
+
+const path = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', join(path, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(join(path, '../public')));
 
 // TODO setja upp rest af virkni!
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
   console.info(`Server running at http://localhost:${port}/`);
+});
+
+/**
+ * Hjálparfall til að athuga hvort reitur sé gildur eða ekki.
+ *
+ * @param {string} field Middleware sem grípa á villur fyrir
+ * @param {array} errors Fylki af villum frá express-validator pakkanum
+ * @returns {boolean} `true` ef `field` er í `errors`, `false` annars
+ */
+
+function isInvalid(field, errors) {
+  return Boolean(errors.find(i => i.param === field));
+}
+
+/**
+ * Hjálparfall sem skilar dagsetningu á lesanlegu formi.
+ *
+ * @param {string} sigDate dagsetning
+ * @returns {string} dagsetning sem hefur verið hreinsað
+ */
+function formatDate(sigDate) {
+  return ((sigDate.toISOString().substring(0, 10)).split('-')).reverse()
+    .join('.');
+} 
+
+app.locals.isInvalid = isInvalid;
+app.locals.formatDate = formatDate;
+app.locals.formatName = formatName;
+
+app.use('/', apply);
+app.use('/registration', registration);
+
+/**
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware sem nota á
+ */
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  res.status(404).render('error', { title: '404', error: '404 fannst ekki' });
+}
+
+/**
+ * @param {object} err Villa sem kom upp í vinnslu
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {object} next næsta middleware sem nota á
+ */
+
+function errorHandler(error, req, res, next) { // eslint-disable-line
+  console.error(error);
+  res.status(500).render('error', { title: 'Villa', error });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const {
+  PORT: port = 3000,
+} = process.env;
+
+app.listen(port, hostname, () => {
+  console.info(`Server running at http://${hostname}:${port}/`);
 });
