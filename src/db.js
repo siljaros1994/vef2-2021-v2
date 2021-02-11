4ed4bfbace567d0bdb6b123afe3ce1dafdfbb180
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-11 21:09:11 +0000

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
 dotenv.config();
 
 const {
@@ -12,4 +15,74 @@ if (!connectionString) {
   process.exit(1);
 }
 
-// TODO gagnagrunnstengingar
+/**
+ * Framkvæmir SQL fyrirspurn á gagnagrunn sem keyrir á `DATABASE_URL`,
+ * skilgreint í `.env`
+ *
+ * @param {string} q Query til að keyra
+ * @param {array} values Fylki af gildum fyrir query
+ * @returns {object} Hlut með niðurstöðu af því að keyra fyrirspurn
+ */
+async function query(q, values = []) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(q, values);
+
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+/**
+ * Bætir við undirskriftir.
+ *
+ * @param {array} data Fylki af gögnum fyrir undirskriftir
+ * @returns {object} Hlut með niðurstöðu af því að keyra fyrirspurn
+ */
+async function insert(data) {
+  const q = `
+INSERT INTO applications
+(name, email, phone, text, job)
+VALUES
+($1, $2, $3, $4, $5)`;
+  const values = [data.name, data.email, data.phone, data.text, data.job];
+
+  return query(q, values);
+}
+
+/**
+ * Sækir allar undirskriftir
+ *
+ * @returns {array} Fylki af öllum undirskriftum
+ */
+async function select() {
+  const result = await query('SELECT * FROM applications ORDER BY id');
+
+  return result.rows;
+}
+
+/**
+ * Uppfærir undirskriftir sem unna.
+ *
+ * @param {string} id Id á undirskriftir
+ * @returns {object} Hlut með niðurstöðu af því að keyra fyrirspurn
+ */
+async function update(id) {
+  const q = `
+UPDATE applications
+SET processed = true, updated = current_timestamp
+WHERE id = $1`;
+
+  return query(q, [id]);
+}
+
+module.exports = {
+  insert,
+  select,
+};
